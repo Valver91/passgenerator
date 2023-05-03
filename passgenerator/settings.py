58cc0d33d9614269dbbaf1d6d16eb916e9e14a6d
Santiago <santiagoferval@gmail.com>
2023-05-03 14:05:57 +0200

    prueba static

--- passgenerator/settings.py
+++ passgenerator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,13 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email config
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = 'f1041aa5c17a32'
-EMAIL_HOST_PASSWORD = '341c2cc664759d'
-EMAIL_PORT = '2525'
