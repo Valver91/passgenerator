02c8632856335ae4320386a1d73060266170f42b
Santiago <santiagoferval@gmail.com>
2023-05-08 14:20:33 +0200

    retoco para static

--- passgenerator/settings.py
+++ passgenerator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h^qrd*tyz&43ms!yivl7n98l#ayad-$tceq-4473a66mxv+z)d
 DEBUG = True
 
 #ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['web-production-e1ed.up.railway.app']
+ALLOWED_HOSTS = ['web-production-e1ed.up.railway.app', '*']
 
 
 # Application definition
@@ -119,10 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
